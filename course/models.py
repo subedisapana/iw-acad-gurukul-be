036976fa88bc8daf04a63142219b130e85fd2ccb
subedisapana <subedisapana10@gmail.com>
2020-08-18 11:22:14 +0545

    model modified

--- course/models.py
+++ course/models.py
@@ -4,7 +4,7 @@ from gurukul.models import UserInfo
 
 class Course(models.Model):
     title = models.CharField(max_length=50)
-    instructor = models.ForeignKey(UserInfo, null=True, limit_choices_to={'is_staff': True}, on_delete=models.SET_NULL)
+    user = models.ManyToManyField(UserInfo, limit_choices_to={'is_staff': True})
 
     def __str__(self):
         return self.title
