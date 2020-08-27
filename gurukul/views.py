29e6ecd77968504697a61f3ea1c14f91d9fd2461
sulav <cyberraid@gmail.com>
2020-08-27 09:38:06 +0545

    [course api]

--- gurukul/views.py
+++ gurukul/views.py
@@ -9,7 +9,6 @@ from rest_framework.authtoken.models import Token
 from .models import UserInfo
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.authentication import TokenAuthentication
-from rest_framework.decorators import permission_classes, authentication_classes
 
 class UserView(APIView):
     permission_classes = [IsAuthenticated]
