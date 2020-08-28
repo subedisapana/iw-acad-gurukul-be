de72fdd1d8beb670502755c62ea52007b3a36bd4
sulav <cyberraid@gmail.com>
2020-08-28 19:11:01 +0545

    [course api updates]

--- course/serializer.py
+++ course/serializer.py
@@ -1,7 +1,22 @@
 from rest_framework import serializers
 from .models import Course
+from assignment.models import Assignment
+from resources.models import Resource
+
+class AssignmentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Assignment
+        fields = ['id', 'title', 'description', 'due_date', 'created_at', 'resource_url'] 
+
+class ResoruceSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Resource
+        fields = ['id', 'title', 'description', 'resource_url', 'posted_at']
 
 class CourseSerializer(serializers.ModelSerializer):
+    assignments = AssignmentSerializer(many=True, read_only=True)
+    resources = ResoruceSerializer(many=True, read_only=True)
     class Meta:
         model = Course
-        fields = ['title', 'assignments']
+        fields = ['title', 'assignments', 'resources']
