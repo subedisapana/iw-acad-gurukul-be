1573bcf9e3225266a23e42c24f08c7458404fc26
sulav <cyberraid@gmail.com>
2020-09-06 19:57:01 +0545

    [deployment-setup]

--- iw_acad_gurukul_be/settings.py
+++ iw_acad_gurukul_be/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool, default=True)
 
-ALLOWED_HOSTS = ['https://nscubegurukul.herokuapp.com/', '127.0.0.1']
+ALLOWED_HOSTS = ['https://nscubegurukul.herokuapp.com', '127.0.0.1', 'nscubegurukul.herokuapp.com']
 
 # Application definition
 
